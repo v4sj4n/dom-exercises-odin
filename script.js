67cd8a214329390395622a1a2f51e9fc88bc10b7
Vasjan <vasjancupri04@gmail.com>
2022-11-15 20:11:24 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+const container = document.getElementById("container");
+
+const pRed = document.createElement('p')
+pRed.textContent = "Hey I'm red";
+pRed.style.color = "red";
+
+container.appendChild(pRed)
