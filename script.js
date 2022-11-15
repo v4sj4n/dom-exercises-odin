4242971297d72df233466e72fc6aad3f945d7642
Vasjan <vasjancupri04@gmail.com>
2022-11-15 20:15:53 +0100

    Finished the 2nd step

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 const container = document.getElementById("container");
 
-const pRed = document.createElement('p')
-pRed.textContent = "Hey I'm red";
+// Red Paragraph
+const pRed = document.createElement("p");
 pRed.style.color = "red";
+pRed.textContent = "Hey I'm red!";
+container.appendChild(pRed);
 
-container.appendChild(pRed)
+// Blue Heading size 3
+const h3Blue = document.createElement("h3");
+h3Blue.style.color = "blue";
+h3Blue.textContent = "I'm a blue h3!";
+container.appendChild(h3Blue)
