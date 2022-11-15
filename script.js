235970390de827effb38325e9c25069eca73333d
Vasjan <vasjancupri04@gmail.com>
2022-11-15 20:26:04 +0100

    Finished the last step

--- script.js
+++ script.js
@@ -1,13 +1,30 @@
 const container = document.getElementById("container");
 
-// Red Paragraph
+//1) step: Add a red paragraph
 const pRed = document.createElement("p");
 pRed.style.color = "red";
 pRed.textContent = "Hey I'm red!";
 container.appendChild(pRed);
 
-// Blue Heading size 3
+//2) step: Add a blue heading size 3
 const h3Blue = document.createElement("h3");
 h3Blue.style.color = "blue";
 h3Blue.textContent = "I'm a blue h3!";
-container.appendChild(h3Blue)
+container.appendChild(h3Blue);
+
+//3) step: Ad a div with a h1 and a p
+const divElem = document.createElement("div");
+divElem.setAttribute(
+  "style",
+  "background-color: pink;border: 1px black solid;"
+);
+
+divElH1 = document.createElement("h1");
+divElH1.textContent = "I'm in a div!";
+divElem.appendChild(divElH1);
+
+divElP = document.createElement("p");
+divElP.textContent = "ME TOO!";
+divElem.appendChild(divElP);
+
+container.appendChild(divElem);
